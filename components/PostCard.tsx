8fd4885bf82f9d62f2c2c1c1f13627f3e9f71cb1
Frank Johnson <frankjohnson1993@gmail.com>
2022-06-22 19:57:18 -0400

    More style tweaks

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -9,8 +9,8 @@ export default function PostCard(post: Post, headingLevel = 3) {
       <div className="">
         <h3 className="mt-0 text-lg font-normal group-hover:text-green-900 first-letter:capitalize">{post.title}</h3>
         <p className="text-sm">
-            <span className={"inline-block mr-2 text-green-800 capitalize stage-before " + post.growthStage}>{ post.growthStage }</span>
-            <time dateTime={post.updated} className="text-sm text-slate-600">
+            <span className={"inline-block text-xs mr-2 text-green-800 capitalize stage-before " + post.growthStage}>{ post.growthStage }</span>
+            <time dateTime={post.updated} className="text-xs text-slate-600">
             Last tended {format(parseISO(post.updated), 'LLLL d, yyyy')}
             </time>
         </p>
