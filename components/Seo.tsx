8383693461b46ac51243b78424549670ccfeb2f1
Frank Johnson <frankjohnson1993@gmail.com>
2022-06-23 21:54:14 -0400

    Give SEO absolute URLs

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,3 +1,4 @@
+import { SITE_ORIGIN } from 'lib/consts';
 import Head from 'next/head';
 import { useRouter } from "next/router";
 
@@ -19,17 +20,17 @@ export default function Seo({ title, description, image }: ISeoProps) {
 
             {/* <!-- Open Graph / Facebook --/> */}
             <meta property="og:type" content="website"/>
-            <meta property="og:url" content={ router.asPath }/>
+            <meta property="og:url" content={ SITE_ORIGIN + router.asPath }/>
             <meta property="og:title" content={ title }/>
             <meta property="og:description" content={ description }/>
-            { image && <meta property="og:image" content={ image }/> }
+            { image && <meta property="og:image" content={ SITE_ORIGIN + image }/> }
 
             {/* <!-- Twitter --/> */}
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content={ router.asPath }/>
             <meta property="twitter:title" content={ title }/>
             <meta property="twitter:description" content={ description }/>
-            { image && <meta property="twitter:image" content={ image }/> }
+            { image && <meta property="twitter:image" content={ SITE_ORIGIN + image }/> }
         </Head>
     )
 }
---
+++ lib/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_ORIGIN: string = (process.env.NODE_ENV === "development")
+    ? "http://localhost:3000"
+    : "https://franknoirot.co"
