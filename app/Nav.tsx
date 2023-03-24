916687fee4811d1d438634f20087ccaac69ea54c
Frank Johnson <frankjohnson1993@gmail.com>
2023-03-24 17:37:58 -0400

    New posts 2023-03-24

---
+++ app/Nav.tsx
@@ -0,0 +1,79 @@
+import { link } from "fs"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import React from "react"
+import Icon from "components/Icon"
+import styles from './Nav.module.css'
+
+type NavLink = {
+    label: string,
+    href: string,
+}
+
+interface INavProps {
+    navLinks: NavLink[],
+}
+
+const socialLinks = [
+    {
+        label: 'Instagram',
+        href: 'https://instagram.com/franknoirot',
+        icon: 'instagram',
+    },
+    {
+        label: 'LinkedIn',
+        href: 'https://linkedin.com/in/frank-johnson-noirot',
+        icon: 'linkedin',
+    },
+    {
+        label: 'Twitter',
+        href: 'https://twitter.com/frank_noirot',
+        icon: 'twitter',
+    },
+]
+
+const Nav: React.FC<INavProps> = ({ navLinks }: { navLinks: NavLink[] }) => {
+    const pathname = usePathname()
+    const getCurrentClasses = (slug: string) => (pathname?.includes(slug))
+        ? 'text-amber-800 bg-amber-100'
+        : ''
+
+    return (
+        <nav className={styles.nav} style={{gridTemplateColumns: 'auto 1fr'}}>
+            <Link href="/" className="block w-8 md:w-10 md:row-span-2 hover:text-amber-500">
+                <Icon type="logo" />
+            </Link>
+            <ul className="flex col-start-2 gap-2">
+                { navLinks.map(page => (
+                    <li key={page.href}>
+                        <Link
+                            href={page.href}
+                            className={'px-2 py-1 hover:bg-amber-200 hover:text-amber-900 rounded-sm ' + getCurrentClasses(page.href)}>
+
+                            { page.label }
+
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <ul className="items-center hidden col-start-2 row-span-2 gap-4 px-2 md:flex">
+                { socialLinks.map(page => (
+                    <li key={page.href}>
+                        <Link
+                            href={page.href}
+                            className={"text-neutral-500 hover:text-amber-600"}
+                            target="_blank"
+                            rel="nofollower noopener">
+
+                            <Icon type={page.icon} width={20} />
+
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav
---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { NextPageWithLayout } from 'lib/utilityTypes'
+import RightClickVisualizer from 'components/RightClickVisualizer'
+import Nav from './Nav'
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function RootLayout({ Component, pageProps }: AppPropsWithLayout) {
+    const navLinks = [
+        { label: 'Writing', href: '/posts' },
+        { label: 'Library', href: '/books' },
+        { label: 'Projects', href: '/projects' },
+        { label: 'About', href: '/about' },
+    ]
+    
+    // Use the layout defined at the page level, if available
+    return (
+        <html lang="en">
+            <head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+                <meta name="msapplication-TileColor" content="#da532c"/>
+                <meta name="theme-color" content="#ffffff"/>
+            </head>
+            <body>
+                <Nav navLinks={navLinks} />
+                <main className="mx-2 mt-10 pb-36 md:mt-0 md:mx-4">
+                    <Component {...pageProps} />
+                </main>
+                <RightClickVisualizer />
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
