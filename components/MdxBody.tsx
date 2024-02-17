5d6922aab0ea30fcc0ed9af3a32a6c6770c2f36a
Frank Noirot <frank@kittycad.io>
2024-02-17 13:28:41 -0500

    Fix types on MdxBody

--- components/MdxBody.tsx
+++ components/MdxBody.tsx
@@ -1,32 +1,50 @@
-import Link from "next/link";
-import BirthdayRSVP from 'components/BirthdayRSVP'
-import Callout from 'components/Callout'
+import Link, { LinkProps } from "next/link";
+import BirthdayRSVP from "components/BirthdayRSVP";
+import Callout from "components/Callout";
 import React from "react";
 import { useMDXComponent } from "next-contentlayer/hooks";
 
-interface ILinkProps extends React.PropsWithChildren {
-    href: string,
-}
-
 interface IImgProps {
-    src: string,
-    alt: string,
+  src: string;
+  alt: string;
 }
 
 export default function MdxBody({ content }: { content: string }) {
-    const MdxComponent = useMDXComponent(content)
+  const MdxComponent = useMDXComponent(content);
 
-    return (
-        <MdxComponent components={{
-            BirthdayRSVP,
-            Callout,
-            a: ({ href, children, ...rest }: ILinkProps) => <Link href={href}><a {...rest}>{ children }</a></Link>,
-            img: ({ src, alt, ...rest }: React.PropsWithoutRef<IImgProps>) => (
-                <img src={((!src.startsWith('http') && !src.startsWith('/assets')) ? '/assets/' : '') + src} className="my-8" {...rest}  alt={alt} />
-            ),
-            
-          }}>
-            {content}
-        </MdxComponent>
-    )
+  return (
+    <MdxComponent
+      components={{
+        BirthdayRSVP,
+        Callout,
+        a: ({
+          href,
+          children,
+          ...rest
+        }: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
+          <Link href={href || ""}>
+            <a {...rest}>{children}</a>
+          </Link>
+        ),
+        img: ({
+          src,
+          alt,
+          ...rest
+        }: React.ImgHTMLAttributes<HTMLImageElement>) => (
+          <img
+            src={
+              (src && !src.startsWith("http") && !src.startsWith("/assets")
+                ? "/assets/"
+                : "") + (src || "")
+            }
+            className="my-8"
+            {...rest}
+            alt={alt}
+          />
+        ),
+      }}
+    >
+      {content}
+    </MdxComponent>
+  );
 }
